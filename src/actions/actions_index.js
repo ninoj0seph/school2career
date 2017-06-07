e435060aa08ab0ca78213a564e435679c5a57b14
Donald Tran <tran.donald@gmail.com>
2017-06-06 22:35:51 -0700

    Changed Axios BASE_URL to point to https//prod

--- src/actions/actions_index.js
+++ src/actions/actions_index.js
@@ -4,18 +4,18 @@ axios.defaults.headers.post['Content-Type'] = 'application/json';
 import { FETCH_SCHOOLS, ONE_SCHOOL, SHOW_MAP, CENTER_COORDS, USER_INPUT, SCHOOL_IMAGE, MAP_BOUNDS_INPUT, SHOW_LOADER } from './actions_types';
 
 // // for live site
-// const BASE_URL = 'https://school2career.net/fetch_schools';
-// const ONESCHOOL_URL = 'https://school2career.net/one_school/id/';
+const BASE_URL = 'https://school2career.net/fetch_schools';
+const ONESCHOOL_URL = 'https://school2career.net/one_school/id/';
 
 // for testing on localhost
-const BASE_URL = 'http://dev.school2career.net/fetch_schools';
-const ONESCHOOL_URL = 'http://dev.school2career.net/one_school/id/';
+// const BASE_URL = 'http://dev.school2career.net/fetch_schools';
+// const ONESCHOOL_URL = 'http://dev.school2career.net/one_school/id/';
 
 //calls the database via axios to get all schools available
 export function searchForSchools(value) {
     value.newInfo=true;
     const newVal = JSON.stringify(value);
-    const request = axios.post(`${BASE_URL}`, newVal)
+    const request = axios.post(`${BASE_URL}`, newVal);
     return {
         type: FETCH_SCHOOLS,
         payload: request
